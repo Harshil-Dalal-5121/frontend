9adba4b4a51f47747284aa7231ad89db51618a92
Harshil-Dalal-5121 <h.chandreshkumar@axelor.com>
2023-03-22 17:05:17 +0530

    Sorting conditions changes

--- app-pos/src/app/index.js
+++ app-pos/src/app/index.js
@@ -50,26 +50,13 @@ function App() {
     let $products;
     if (!sort) {
       $products = [...products].sort((a, b) => {
-        if (a[name] < b[name]) {
-          return -1;
-        }
-        if (a[name] > b[name]) {
-          return 1;
-        }
-        return 0;
+        return a[name] < b[name] ? -1 : 1;
       });
       setProducts($products);
     } else {
       $products = [...products].sort((a, b) => {
-        if (a[name] < b[name]) {
-          return 1;
-        }
-        if (a[name] > b[name]) {
-          return -1;
-        }
-        return 0;
+        return a[name] < b[name] ? 1 : -1;
       });
-
       setProducts($products);
     }
   };
