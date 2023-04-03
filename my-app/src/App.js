8aaddaecce9272a27f660ffa296daf1b378d264a
Harshil Chandreshkumar Dalal <h.chandreshkumar@axelor.com>
2023-04-03 18:53:22 +0530

    Sending data to Api trial

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,17 @@
+// name,
+// fromDate,
+// projectStatus,
+// parentProject,
+// clientPartner,
+// toDate,
+// imputable,
+// assignedTo,
+// code
+//
 import React, { useEffect, useState } from "react";
 import { Check, XCircle } from "react-bootstrap-icons";
 import {
+  Button,
   Paper,
   Table,
   TableBody,
@@ -24,38 +35,77 @@ const tableFields = [
   "code",
 ];
 
-const auth = async () => {
-  const data = await axios.post("/callback", {
-    username: "admin",
-    password: "admin",
-  });
-  localStorage.setItem("csrf-token", data.headers["x-csrf-token"]);
+// const auth = async () => {
+//   const data = await axios.post("/callback", {
+//     username: "admin",
+//     password: "admin",
+//   });
+//   localStorage.setItem("csrf-token", data.headers["x-csrf-token"]);
+// };
+
+const intialValues = {
+  name: "",
+  fromDate: "",
+  projectStatus: "",
+  parentProject: "",
+  clientPartner: "",
+  toDate: "",
+  imputable: "",
+  assignedTo: "",
+  code: "",
 };
 
 const App = () => {
-  const [projects, setProjects] = useState([]);
-  useEffect(() => {
-    auth();
-  }, []);
+  const [projects, setProjects] = useState();
+  const [newproject, setNewProject] = useState(intialValues);
 
-  useEffect(() => {
-    const token = localStorage.getItem("csrf-token");
-    axios
-      .post(
-        "ws/rest/com.axelor.apps.project.db.Project/search",
-        {
-          fields: tableFields,
-        },
-        {
-          headers: {
-            "X-CSRF-TOKEN": token,
-          },
-        }
-      )
-      .then((reponse) => {
-        setProjects(reponse.data.data);
-      });
-  }, []);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewProject({ ...newproject, [name]: value });
+  };
+
+  const rest = axios.create({
+    headers: {
+      Authorization: "Basic YWRtaW46YWRtaW4=",
+    },
+  });
+
+  // useEffect(() => {
+  //   auth();
+  // }, []);
+
+  // useEffect(() => {
+  //   const token = localStorage.getItem("csrf-token");
+  //   axios
+  //     .post(
+  //       "ws/rest/com.axelor.apps.project.db.Project/search",
+  //       {
+  //         fields: tableFields,
+  //       },
+  //       {
+  //         headers: {
+  //           "X-CSRF-TOKEN": token,
+  //         },
+  //       }
+  //     )
+  //     .then((reponse) => {
+  //       setProjects(reponse.data.data);
+  //     });
+  // }, []);
+
+  const saveProject = () => {
+    let data = newproject;
+
+    console.log(data);
+    rest.post("ws/rest/com.axelor.apps.project.db.Project", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: data,
+    });
+  };
 
   return (
     <>
@@ -74,6 +124,9 @@ const App = () => {
               <TableCell align="center">To Date</TableCell>
               <TableCell align="center">Imputable</TableCell>
               <TableCell align="center">Status</TableCell>
+              <TableCell align="center" colSpan={"2"}>
+                Operations
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -83,14 +136,14 @@ const App = () => {
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="project">
-                  {project.id}
+                  {project?.id}
                 </TableCell>
                 <TableCell component="th" scope="project">
-                  {project.name || "-"}
+                  {project?.name || "-"}
                 </TableCell>
                 <TableCell align="center">{project.code || "-"}</TableCell>
                 <TableCell align="center">
-                  {project.parentProject || "-"}
+                  {project?.parentProject || "-"}
                 </TableCell>
                 <TableCell align="center">
                   {project.clientPartner?.fullName || "-"}
@@ -98,19 +151,125 @@ const App = () => {
                 <TableCell align="center">
                   {project.assignedTo?.fullName || "-"}
                 </TableCell>
-                <TableCell align="center">{project.fromDate || "-"}</TableCell>
-                <TableCell align="center">{project.toDate || "-"}</TableCell>
+                <TableCell align="center">{project?.fromDate || "-"}</TableCell>
+                <TableCell align="center">{project?.toDate || "-"}</TableCell>
                 <TableCell align="center">
-                  {project.imputable === true ? <Check /> : <XCircle />}
+                  {project?.imputable === true ? <Check /> : <XCircle />}
                 </TableCell>
                 <TableCell align="center">
                   {project.projectStatus?.name}
+                </TableCell>
+
+                <TableCell align="center">
+                  <Button variant="contained" color="success" id={project.id}>
+                    Update
+                  </Button>
+                </TableCell>
+                <TableCell align="center">
+                  <Button variant="contained" color="success">
+                    Delete
+                  </Button>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+
+      <div style={{ padding: "50px" }}>
+        <form>
+          <table border={`2`}>
+            <tr>
+              <td>Name :</td>
+              <td>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Code :</td>
+              <td>
+                <input
+                  type="text"
+                  name="code"
+                  id="code"
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Parent Project :</td>
+              <td>
+                <input
+                  type="text"
+                  name="parentProject"
+                  id="parentProject"
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Customer :</td>
+              <td>
+                <input
+                  type="text"
+                  name="customer"
+                  id="customer"
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Assigned To :</td>
+              <td>
+                <input
+                  type="text"
+                  name="assignedTo"
+                  id="assignedTo"
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>From Date :</td>
+              <td>
+                <input
+                  type="date"
+                  name="fromTo"
+                  id="fromTo"
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>To Date :</td>
+              <td>
+                <input
+                  type="date"
+                  name="toDate"
+                  id="toDate"
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={"2"}>
+                <Button
+                  variant="contained"
+                  color="success"
+                  onClick={saveProject}
+                >
+                  Update
+                </Button>
+              </td>
+            </tr>
+          </table>
+        </form>
+      </div>
     </>
   );
 };
